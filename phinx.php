8fbcb3bede5f9e40c867d352bfda1e0eb4971237
Ilya Tearo <itearo@gmail.com>
2020-08-30 13:33:47 +0500

    Установил phinx для использования миграций и сидов БД

---
+++ phinx.php
@@ -0,0 +1,45 @@
+<?php
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+function parseAgnosticDsn(string $dsn): ?array
+{
+    $regex = '#^(?P<adapter>[^\\:]+)\\://(?:(?P<user>[^\\:@]+)(?:\\:(?P<pass>[^@]*))?@)?'
+        . '(?P<host>[^\\:@/]+)(?:\\:(?P<port>[1-9]\\d*))?/(?P<name>[^\?]+)(?:\?(?P<query>.*))?$#';
+
+    if (preg_match($regex, trim($dsn), $parsedOptions)) {
+        $additionalOpts = [];
+        if (isset($parsedOptions['query'])) {
+            parse_str($parsedOptions['query'], $additionalOpts);
+        }
+        $validOptions = ['adapter', 'user', 'pass', 'host', 'port', 'name'];
+        $parsedOptions = array_filter(array_intersect_key($parsedOptions, array_flip($validOptions)));
+        return array_merge($additionalOpts, $parsedOptions);
+    }
+
+    return null;
+}
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/.env');
+
+$appDsn = getenv('DATABASE');
+
+return
+    [
+        'paths' => [
+            'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+            'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+        ],
+        'environments' => [
+            'default_migration_table' => 'phinxlog',
+            'default_environment' => 'app',
+            'app' => parseAgnosticDsn($appDsn),
+        ],
+        'version_order' => 'execution',
+        'templates' => [
+            'file' => __DIR__ . '/db/MigrationClass.template',
+        ],
+    ];
--- src/Provider/ConsoleCommandProvider.php
+++ src/Provider/ConsoleCommandProvider.php
@@ -9,23 +9,23 @@ use App\Command\RouteListCommand;
 use App\Support\CommandMap;
 use App\Support\ServiceProviderInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Phinx\Console\Command\Breakpoint;
+use Phinx\Console\Command\Create;
+use Phinx\Console\Command\Migrate;
+use Phinx\Console\Command\Rollback;
+use Phinx\Console\Command\SeedCreate;
+use Phinx\Console\Command\SeedRun;
+use Phinx\Console\Command\Status;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Client\ClientInterface;
 use Psr\Log\LoggerInterface;
 use Slim\Interfaces\RouteCollectorInterface;
+use Symfony\Component\Console\Command\Command;
 use UltraLite\Container\Container;
 
-/**
- * Class ConsoleCommandProvider.
- */
 class ConsoleCommandProvider implements ServiceProviderInterface
 {
-    /**
-     * @param Container $container
-     *
-     * @return mixed|void
-     */
-    public function register(Container $container)
+    public function register(Container $container): void
     {
         $container->set(RouteListCommand::class, static function (ContainerInterface $container) {
             return new RouteListCommand($container->get(RouteCollectorInterface::class));
@@ -37,5 +37,28 @@ class ConsoleCommandProvider implements ServiceProviderInterface
 
         $container->get(CommandMap::class)->set(RouteListCommand::getDefaultName(), RouteListCommand::class);
         $container->get(CommandMap::class)->set(FetchDataCommand::getDefaultName(), FetchDataCommand::class);
+
+        $this->registerPhinxCommands($container, 'phinx:');
+    }
+
+    private function registerPhinxCommands(Container $container, string $phinxPrefix): void
+    {
+        $commands = [
+            Create::class,
+            Migrate::class,
+            Rollback::class,
+            Status::class,
+            Breakpoint::class,
+            SeedCreate::class,
+            SeedRun::class,
+        ];
+
+        foreach ($commands as $commandClassName) {
+            /** @var Command|string $commandClassName */
+            $commandDefaultName = $commandClassName::getDefaultName();
+
+            $container->set($commandClassName, fn() => new $commandClassName($phinxPrefix . $commandDefaultName));
+            $container->get(CommandMap::class)->set($phinxPrefix . $commandDefaultName, $commandClassName);
+        }
     }
 }
